f9cb26b6b840769d75995d13c499831864ac8019
Jacob Doren <Jacob Doren>
2021-09-20 19:21:26 -0400

    cleaning up code

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,4 @@
 const router = require('express').Router();
-const User = require('../models/User')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 const userController = require('../controllers/userController')
 const { registerValidator, loginValidator, loggedInValidator } = require('../middleware/userValidator')
 
@@ -9,27 +6,16 @@ const { registerValidator, loginValidator, loggedInValidator } = require('../mid
 // Register
 router.post('/', registerValidator, userController.registerUser)
 
+// Login
 router.post('/login', loginValidator, userController.registerUser)
 
+// Logout
 router.get("/logout", (req, res) => {
     res.cookie("token", "", { httpOnly: true, expires: new Date(0) }).send();
 })
 
+// Determine if logged in
 router.get('/loggedin', loggedInValidator, userController.getLoggedIn)
 
-// router.get('/loggedin', (req, res) => {
-//     try {
-//         const token = req.cookies.token;
-        
-//         if(!token) return res.json(false);
-
-//         jwt.verify(token, process.env.JWT_SECRET);
-
-//         res.send(true);
-
-//     } catch(err) {
-//         res.json(false);
-//     }
-// })
 
 module.exports = router;
--- services/userService.js
+++ services/userService.js
@@ -7,14 +7,10 @@ exports.registerUser = async (userInfo) => {
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt)
 
-    const newUser = new User({
-        email, passwordHash
-    })
-
+    const newUser = new User({ email, passwordHash })
     const savedUser = await newUser.save();
 
     return jwt.sign({ user: savedUser._id }, process.env.JWT_SECRET)
-
 }
 
 exports.loginUser = async (userInfo) => {
