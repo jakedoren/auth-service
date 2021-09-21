cba3f2d8537b3243f9767cba1e0036a31c673544
Jacob Doren <Jacob Doren>
2021-09-20 20:07:50 -0400

    jsdocs

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,10 +1,19 @@
 const noteService = require('../services/noteService')
 
+/**
+ * Create the users note.
+ * @param {Object} req.body - The user's note they wish to create (title, body).
+ * @param {Object} req.user - The hashed and salted document id of the logged in user.
+ */
 exports.createNote = async (req, res) => {
     const createdNote = await noteService.createNote(req.body, req.user)
     res.json(`The following note successfully created ${createdNote}`)
 }
 
+/**
+ * Get the users notes.
+ * @param {Object} req.user - The hashed and salted document id of the logged in user.
+ */
 exports.getNote = async (req, res) => {
     const yourNotes = await noteService.getNotes(req.user)
     res.json(yourNotes)
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,26 @@
 const userService = require('../services/userService')
 
+/**
+ * Register the user.
+ * @param {Object} req.body - The user's registration imformation (email, password, passwordVerify).
+ * @param {string} token - Token is the hashed and salted document id of the newly registered user.
+ */
 exports.registerUser = async (req, res) => {
     const token = await userService.registerUser(req.body)
     res.cookie("token", token, { httpOnly: true }).send();
 }
 
+/**
+ * Login the user.
+ * @param {Object} req.body - The user's registration imformation (email, password).
+ * @param {string} token - Token is the hashed and salted document id of the newly registered user.
+ */
 exports.loginUser = async (req, res) => {
     const token = userService.loginUser(req.body)
     res.cookie("token", token, { httpOnly: true }).send()
 }
 
+// Determing if user is logged in
 exports.getLoggedIn = async (req, res) => {
     res.json(true)
 }
